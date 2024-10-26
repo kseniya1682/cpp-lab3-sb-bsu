9331872d0ad807e8e6d64eb151e0ae03bfc6a376
kseniya1682 <kseniyatruhan07@gmail.com>
2024-10-26 22:30:20 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,15 +1,22 @@
 #include<iostream> 
-using namespace std; 
+using namespace std;
 
 int main()
 {
-int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": "; 
-cin >> number;
-sum_k += number; i++;
-} while (i <= k);
-cout << " The total sum of " << k << " integers is: "  << sum_k;
-return 0;
+	int k, i = 1, sum_k = 0, number;
+	cout << " Enter the amount of integers to sum = "; cin >> k;
+	if (k != 0)
+	{
+		do {
+			cout << "Enter integer nr. " << i << ": ";
+			cin >> number;
+			sum_k += number; i++;
+		} while (i <= k);
+		cout << " The total sum of " << k << " integers is: " << sum_k;
+	}
+	else
+	{
+		cout << " Error! The total sum of 0 integers is cannot be calculated! ";
+	}
+	return 0;
 }
